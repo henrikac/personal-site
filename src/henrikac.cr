0300f8bdde101683c559df7ac46d422a9b7d92af
Henrik Christensen <sensen1695@hotmail.com>
2021-06-25 00:40:53 +0200

    repos are now being rendered in template

--- src/henrikac.cr
+++ src/henrikac.cr
@@ -1,4 +1,7 @@
 require "kemal"
+require "./**"
+
+repos = GitHub.fetch_repos(["pokeapi", "prettytable", "lib-giphy", "user-dirs"])
 
 get "/" do
   render "src/views/index.ecr", "src/views/layout.ecr"
