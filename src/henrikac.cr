e195986b87cb66c792b5b930e848f56bb8da3d08
Henrik Christensen <sensen1695@hotmail.com>
2021-06-25 14:33:49 +0200

    added fiber to fetch repo data

--- src/henrikac.cr
+++ src/henrikac.cr
@@ -1,7 +1,18 @@
 require "kemal"
 require "./**"
 
-repos = GitHub.fetch_repos(["pokeapi", "prettytable", "lib-giphy", "user-dirs"])
+mut = Mutex.new
+repos = Array(GitHub::Repo).new
+
+spawn do
+  loop do
+    mut.lock
+    repos = GitHub.fetch_repos(["pokeapi", "prettytable", "lib-giphy", "user-dirs"])
+    mut.unlock
+
+    sleep 5.minutes
+  end
+end
 
 get "/" do
   render "src/views/index.ecr", "src/views/layout.ecr"
